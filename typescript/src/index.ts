9291344e2ebe67775ca7472228ca2e1b4779a4bd
sebssgarcia502580 <sebssgarcia502580@gmail.com>
2026-04-12 01:36:40 +0200

    refactor: remove deprecated typescript sdk package

--- typescript/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./contracts.js";
-export * from "./game-schemas.js";
-export * from "./proxy.js";
-export * from "./ai-engine-client.js";
-export * from "./game-categories.js";
-export * from "./private-docs.js";
